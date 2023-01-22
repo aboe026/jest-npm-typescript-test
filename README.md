a97a05dfe7f0f4b6f8221fdfb94069b6de64bae4
Adam Boe <adamboe@outlook.com>
2023-01-21 22:41:46 -0800

    MVP

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jest-npm-typescript-test
+
 A repository to test jest working with npm and typescript
---
+++ test/file-util.test.ts
@@ -0,0 +1,18 @@
+import fs from 'fs-extra'
+
+import FileUtil from '../src/file-util'
+
+describe('File Util Unit Tests', () => {
+  describe('pathExists', () => {
+    it('returns false if fs access throws error', async () => {
+      jest.spyOn(fs, 'access').mockImplementation((path, callback) => {
+        callback(new Error('Permission Denied.'))
+      })
+      await expect(FileUtil.pathExists('toast')).resolves.toEqual(false)
+    })
+    it('returns true if fs access does not throw error', async () => {
+      jest.spyOn(fs, 'access').mockImplementation()
+      await expect(FileUtil.pathExists('toast')).resolves.toEqual(true)
+    })
+  })
+})
