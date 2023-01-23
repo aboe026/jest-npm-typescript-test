61d4a93f173a56ef4d260ed0dd3455c60fae39c8
Adam Boe <adamboe@outlook.com>
2023-01-22 17:31:29 -0800

    Lint ingore ilne

--- test/file-util.test.ts
+++ test/file-util.test.ts
@@ -5,6 +5,7 @@ import FileUtil from '../src/file-util'
 describe('File Util Unit Tests', () => {
   describe('pathExists', () => {
     it('returns false if fs access throws error', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       jest.spyOn(fs, 'access').mockImplementation((path, callback) => {
         callback(new Error('Permission Denied.'))
       })
